65cd8fde7e0fa6016ea2827512d3073b19556804
Ujjwal <ujjwaljha744@gmail.com>
2024-05-31 20:34:25 +0530

    Updated

---
+++ JavaScript/hello.js
@@ -0,0 +1,3 @@
+console.log('Hello World!')
+console.log("My name is Ujjwal")
+console.log(7+3);
---
+++ JavaScript/variables.js
@@ -0,0 +1,10 @@
+/**
+ * Lesson - 2
+ * Why Variables 🚀
+ * What is a Variable 🔥
+ * Different ways to create variables ⭐
+ */
+
+var message;
+message="Hello world!";
+console.log(message);
